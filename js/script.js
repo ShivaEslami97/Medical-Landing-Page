08cdd1c56b73082a03e6164a3ec6a1913b7e8533
Shiva Eslami <shivaslami@gmail.com>
2022-05-07 00:41:01 +0430

    Update animation and hover css

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
-'use strict';
 
+
+////// sticky menu
+window.onscroll = function () {
+    myFunction()
+};
+var header = document.getElementById("header");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("is-sticky");
+    } else {
+        header.classList.remove("is-sticky");
+    }
+}
+
+///////// back to top
+const showOnPx = 100;
+const backToTopButton = document.querySelector(".back-to-top");
+const goToTop = () => {
+    document.body.scrollIntoView({
+        behavior: "smooth"
+    });
+};
+document.addEventListener("scroll", () => {
+    let scrollPosition = window.pageYOffset;
+    if (scrollPosition >= showOnPx) {
+        backToTopButton.classList.remove("hidden");
+    } else {
+        backToTopButton.classList.add("hidden");
+    }
+});
+
+backToTopButton.addEventListener("click", goToTop);
+////// tab js
 const tabItemList = document.querySelectorAll('.tab__item');
 const tabContentList = document.querySelectorAll('.tab__content');
 
